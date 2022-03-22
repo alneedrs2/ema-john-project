38ea39fa6be4ce84c7bade2ff8b47aa35d5e71fa
Alneed Roy <alneedrs2@gmail.com>
2022-03-23 02:45:33 +0600

    fixed cart summary

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
 import React from "react";
+import("./Cart.css");
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
+    shipping = shipping + product.shipping;
+  }
+  const taxString = (total * 0.1).toFixed(2);
+  const tax = parseFloat(taxString);
+  const grandTotal = total + shipping + tax;
   return (
-    <div>
+    <div className="cart">
       <h3 className="order">Order Summary</h3>
       <div className="order-info">
-        <p>Selected Items: {cart.length}</p>
-        <p>Total Price: ${}</p>
-        <p>Total Shipping Charge: ${}</p>
-        <p>Tax: ${}</p>
-        <h4>Grand Total: ${}</h4>
+        <p>Selected Items: {quantity}</p>
+        <p>Total Price: ${total}</p>
+        <p>Total Shipping Charge: ${shipping}</p>
+        <p>Tax: ${tax}</p>
+        <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
       </div>
     </div>
   );
